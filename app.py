32f0ab82a75ffb5f930515a846f578221692bea3
venkateshkumar26 <plpvk4951@gmail.com>
2025-12-24 17:26:59 +0530

    committed after containerization

---
+++ app.py
@@ -0,0 +1,28 @@
+import joblib
+from fastapi import FastAPI
+import numpy as np
+
+app=FastAPI()
+
+model=joblib.load("models/model.pkl")
+
+THRESHOLD=0.2
+
+from pydantic import BaseModel
+
+class Input(BaseModel):
+    features:list[float]
+
+@app.post("/fraud_detector")
+def predict(features:Input):
+    X=np.array(features.features).reshape(1,-1)
+    if len(X)!=30:
+        return {"error":"Expected 30 features"}
+    prob=model.predict_proba(X)[0][1]
+    pred=int(prob>=THRESHOLD)
+    return {
+        "fraud_probability":round(float(prob),4),
+        "prediction":"fraud" if pred==1 else "not_fraud",
+        "threshold_used":THRESHOLD
+    }
+    
